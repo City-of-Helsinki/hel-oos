37630f3a48c43c06813aad151d3664dc83109a9f
Elias Ylänen <elias.ylanen@gmail.com>
2017-06-27 16:04:16 +0300

    Refactor markup

--- web/app/themes/pops/templates/header-blog.php
+++ web/app/themes/pops/templates/header-blog.php
@@ -9,24 +9,23 @@
 ?>
 
 <!doctype html>
-<html class="no-js" <?php language_attributes();
-																				?>>
+<html class="no-js" <?php language_attributes(); ?>>
 <head>
 
-	<meta charset="<?php echo get_bloginfo('charset');
-																?>"/>
+	<meta charset="<?php echo get_bloginfo('charset'); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="theme-color" content="#F5A3C7" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
 	<title><?php wp_title('|', true, 'right'); ?></title>
 
 	<!-- ============================================================ -->
-	<?php wp_head();
-	?>
+	<?php wp_head(); ?>
 	<!-- ============================================================ -->
 
-	<meta name="theme-color" content="#F5A3C7" />
 </head>
 
 <body <?php body_class(); ?>>
