62969771cbec453c7e0302ad6b64cce20a95dcd0
Elias Ylänen <elias.ylanen@gmail.com>
2017-06-27 13:24:58 +0300

    Add favicons; remove header logo img

--- web/app/themes/pops/templates/header.php
+++ web/app/themes/pops/templates/header.php
@@ -9,33 +9,39 @@
 ?>
 
 <!doctype html>
-<html class="no-js" <?php language_attributes(); ?>>
+<html class="no-js" <?php language_attributes();
+																				?>>
 <head>
 
-	<meta charset="<?php echo get_bloginfo( 'charset' ); ?>"/>
+	<meta charset="<?php echo get_bloginfo('charset');
+																?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<title><?php wp_title('|', true, 'right');
+								?></title>
 
 	<!-- ============================================================ -->
-	<?php wp_head(); ?>
+	<?php wp_head();
+	?>
 	<!-- ============================================================ -->
 
 </head>
 
-<body <?php body_class(); ?>>
-	<?php do_action( 'pops_after_body' ); ?>
+<body <?php body_class();
+						?>>
+	<?php do_action('pops_after_body');
+	?>
 
 	<header class="header">
 		<section class="row medium-container">
 			<div class="header__logo">
-				<img src="<?php echo UTILS()->get_image_uri(); ?>/oos.png" />
 				<h3>OOS</h3>
-				<a href="https://youtu.be/uG136mmSrwE" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+				<a href="https://www.youtube.com/playlist?list=PLHrkP0ZaJoKD8sdCQ2GfMQpYwLpdwVKYI" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
 			</div>
 			<nav class="header__menu">
-				<?php pops_main_menu(); ?>
+				<?php pops_main_menu();
+				?>
 			</nav>
 		</section>
