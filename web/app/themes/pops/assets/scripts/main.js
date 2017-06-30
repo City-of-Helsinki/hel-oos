f3dee6450515e982f8c04325f8595bc7910a6600
Elias Ylänen <elias.ylanen@gmail.com>
2017-06-30 11:19:56 +0300

    Mayhaps fix modal on Safari

--- web/app/themes/pops/assets/scripts/main.js
+++ web/app/themes/pops/assets/scripts/main.js
@@ -7,12 +7,14 @@ import 'jquery-match-height';
 // import local dependencies
 import Router from './util/router';
 import common from './routes/Common';
+import home from './routes/Home';
 
 // Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
 const routes = {
   // All pages
   common,
+  home,
 };
 
 // Load Events
