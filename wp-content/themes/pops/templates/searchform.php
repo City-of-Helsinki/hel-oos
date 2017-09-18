42c5e157a8589e6cfef15b33af97349983ba6092
Timo Leppanen <timo@luuptek.fi>
2017-09-18 12:13:20 +0300

    fix translation function to include text domain

--- wp-content/themes/pops/templates/searchform.php
+++ wp-content/themes/pops/templates/searchform.php
@@ -12,5 +12,5 @@
     <div class="form-group">
         <input type="text" name="s" id="s" class="oos-search-form-control" placeholder="<?php _e( 'Input search terms', TEXT_DOMAIN ); ?>">
     </div>
-    <button type="submit" class="btn btn-search"><?php echo esc_attr_x( 'Search', 'search button' ); ?></button>
+    <button type="submit" class="btn btn-search"><?php echo esc_attr_x( 'Search', 'search button', TEXT_DOMAIN ); ?></button>
 </form>
