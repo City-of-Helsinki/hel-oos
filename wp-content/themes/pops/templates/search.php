9ad180ac25c991b2c791dfb4fdba101713e8d5e3
Timo Leppanen <timo@luuptek.fi>
2017-09-18 10:33:44 +0300

    rewrite search and searchform

--- wp-content/themes/pops/templates/search.php
+++ wp-content/themes/pops/templates/search.php
@@ -1,25 +1,52 @@
 <?php
 
 /**
- * The main search-wrapper
+ * The main search wrapper
  *
  * @package POPS
  */
 
-get_header();
+get_header( 'blog' );
 
 ?>
 
-<?php do_action( 'pops_before_page' ); ?>
 
-<?php if ( have_posts() ) : ?>
-	<?php printf( __( 'Search Results for: %s', TEXT_DOMAIN ), '<span>' . get_search_query() . '</span>' ); ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php get_template_part( 'partials/content', 'search' ); ?>
-	<?php endwhile; ?>
+<section class="row main-container text-center">
+	<div class="header-container">
+		<h1 class="margin-bottom-container"><?php _e( 'Search results with keyword', TEXT_DOMAIN ); ?></h1>
+		<p class="header__ingress"><?php echo get_search_query() ?></p>
+	</div>
+	<div class="blog-search">
+		<h2 class="search-toggle">
+			<i class="fa fa-search" aria-hidden="true"></i>
+		</h2>
+		<div class="search-container">
+			<?php get_search_form(); ?>
+		</div>
+	</div>
+</section>
+</header>
 
-<?php else : ?>
-	<?php get_template_part( 'partials/no-results', 'search' ); ?>
-<?php endif; ?>
+<style>
+	.wave {
+		background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
+	}
+</style>
 
-<?php get_footer(); ?>
+<div class="wave"></div>
+
+<main class="blog-archive">
+	<section class="container-fluid">
+		<?php
+		if ( have_posts() ) :
+			echo do_shortcode( '[ajax_load_more post_type="post" posts_per_page="4" button_label="Lataa lisää"]' );
+		else:
+			get_template_part('partials/no-results', 'search');
+		endif;
+		?>
+	</section>
+</main>
+
+
+<?php get_footer();
+?>
--- wp-content/themes/pops/templates/searchform.php
+++ wp-content/themes/pops/templates/searchform.php
@@ -8,11 +8,9 @@
 
 ?>
 
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div>
-        <label class="show-for-sr" for="s"><?php echo _x( 'Search for:', 'label' ); ?></label>
-        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s"
-               placeholder="<?php _e( 'Input search terms', TEXT_DOMAIN ); ?>"/>
-        <button type="submit"><?php echo esc_attr_x( 'Search', 'submit button' ); ?> <i class="fa fa-search"></i></button>
+<form class="form-inline" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <div class="form-group">
+        <input type="text" name="s" id="s" class="oos-search-form-control" placeholder="<?php _e( 'Input search terms', TEXT_DOMAIN ); ?>">
     </div>
+    <button type="submit" class="btn btn-search"><?php echo esc_attr_x( 'Search', 'search button' ); ?></button>
 </form>
