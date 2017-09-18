c309c2459fbeff7db179f851714f24de7cb430bf
Timo Leppanen <timo@luuptek.fi>
2017-09-18 10:33:22 +0300

    modify index.php to include search fields

--- wp-content/themes/pops/templates/index.php
+++ wp-content/themes/pops/templates/index.php
@@ -6,43 +6,44 @@
  * @package POPS
  */
 
-get_header('blog');
+get_header( 'blog' );
 
 ?>
 
 
-	<section class="row main-container text-center">
-			<div class="header-container">
-				<h1 class="margin-bottom-container"><?php the_field('header', get_option('page_for_posts')); ?></h1>
-				<p class="header__ingress"> <?= wp_strip_all_tags(get_field('subheader', get_option('page_for_posts'))); ?> </p>
-			</div>
-		</section>
+<section class="row main-container text-center">
+    <div class="header-container">
+        <h1 class="margin-bottom-container"><?php the_field( 'header', get_option( 'page_for_posts' ) ); ?></h1>
+        <p class="header__ingress"> <?= wp_strip_all_tags( get_field( 'subheader', get_option( 'page_for_posts' ) ) ); ?> </p>
+    </div>
+    <div class="blog-search">
+        <h2 class="search-toggle">
+            <i class="fa fa-search" aria-hidden="true"></i>
+        </h2>
+        <div class="search-container">
+	        <?php get_search_form(); ?>
+        </div>
+    </div>
+</section>
 </header>
 
 <style>
-  .wave {
-    background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
-  }
+    .wave {
+        background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
+    }
 </style>
 
 <div class="wave"></div>
 
 <main class="blog-archive">
-	<section class="container-fluid">
+    <section class="container-fluid">
 		<?php
-		if (have_posts()) :
-			echo do_shortcode('[ajax_load_more post_type="post" posts_per_page="4" button_label="Lataa lisää"]');
+		if ( have_posts() ) :
+			echo do_shortcode( '[ajax_load_more post_type="post" posts_per_page="4" button_label="Lataa lisää"]' );
 		endif;
 		?>
-	</section>
+    </section>
 </main>
-
-
-
-
-
-
-
 
 
 <?php get_footer();
