a0dc2193d3e3e935f49f380520ec7e0ac09e72fe
Timo Leppanen <timo@luuptek.fi>
2017-09-18 10:52:34 +0300

    rest of the search pages

--- wp-content/themes/pops/partials/no-results-search.php
+++ wp-content/themes/pops/partials/no-results-search.php
@@ -10,8 +10,7 @@
 ?>
 
 <article>
-	<?php printf( __( 'No results found for %s', TEXT_DOMAIN ), '<span>' . get_search_query() . '</span>' ); ?>
-	<?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.',
-		TEXT_DOMAIN ); ?>
+	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.',
+		TEXT_DOMAIN ); ?></p>
 	<?php get_search_form(); ?>
 </article>
--- wp-content/themes/pops/templates/search.php
+++ wp-content/themes/pops/templates/search.php
@@ -12,39 +12,38 @@ get_header( 'blog' );
 
 
 <section class="row main-container text-center">
-	<div class="header-container">
-		<h1 class="margin-bottom-container"><?php _e( 'Search results with keyword', TEXT_DOMAIN ); ?></h1>
-		<p class="header__ingress"><?php echo get_search_query() ?></p>
-	</div>
-	<div class="blog-search">
-		<h2 class="search-toggle">
-			<i class="fa fa-search" aria-hidden="true"></i>
-		</h2>
-		<div class="search-container">
+    <div class="header-container">
+        <p class="header__ingress"><?php echo __( 'Search results with keyword', TEXT_DOMAIN ) . ': ' . get_search_query() ?></p>
+    </div>
+    <div class="blog-search">
+        <h2 class="search-toggle">
+            <i class="fa fa-search" aria-hidden="true"></i>
+        </h2>
+        <div class="search-container">
 			<?php get_search_form(); ?>
-		</div>
-	</div>
+        </div>
+    </div>
 </section>
 </header>
 
 <style>
-	.wave {
-		background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
-	}
+    .wave {
+        background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
+    }
 </style>
 
 <div class="wave"></div>
 
 <main class="blog-archive">
-	<section class="container-fluid">
+    <section class="container-fluid">
 		<?php
 		if ( have_posts() ) :
-			echo do_shortcode( '[ajax_load_more post_type="post" posts_per_page="4" button_label="Lataa lisää"]' );
+			echo do_shortcode( '[ajax_load_more post_type="post" posts_per_page="4" search="' . get_search_query() . '" button_label="Lataa lisää"]' );
 		else:
-			get_template_part('partials/no-results', 'search');
+			get_template_part( 'partials/no-results', 'search' );
 		endif;
 		?>
-	</section>
+    </section>
 </main>
 
 
