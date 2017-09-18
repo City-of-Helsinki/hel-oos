7f68d6077238e8674b582570ddebe38527d93d37
Timo Leppanen <timo@luuptek.fi>
2017-09-18 13:03:59 +0300

    create 404 page

--- wp-content/themes/pops/templates/404.php
+++ wp-content/themes/pops/templates/404.php
@@ -1,15 +1,40 @@
 <?php
 
 /**
- * The main 404-wrapper
+ * 404 wrapper
  *
  * @package POPS
  */
 
-get_header();
+get_header( 'blog' );
 
 ?>
 
-<?php do_action( 'pops_before_page' ); ?>
-<?php get_template_part( 'partials/no-results', '404' ); ?>
-<?php get_footer(); ?>
+
+<section class="row main-container text-center">
+	<div class="header-container">
+		<p class="header__ingress"><?php _e( 'Error 404: Page not found', TEXT_DOMAIN ); ?></p>
+	</div>
+</section>
+</header>
+
+<style>
+	.wave {
+		background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_blog.svg) repeat-x center;
+	}
+</style>
+
+<div class="wave"></div>
+
+<main class="blog-archive">
+	<section class="container-fluid">
+		<article>
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', TEXT_DOMAIN ); ?></p>
+			<?php get_search_form(); ?>
+		</article>
+	</section>
+</main>
+
+
+<?php get_footer();
+?>
