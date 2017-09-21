c5416a8a84f1f23c38f475a5b0dfd498176ec2ce
Timo Leppanen <timo@luuptek.fi>
2017-09-21 20:16:08 +0300

    more features to front page

--- wp-content/themes/pops/custom-templates/home.tpl.php
+++ wp-content/themes/pops/custom-templates/home.tpl.php
@@ -11,80 +11,87 @@
 
 get_header();
 
-do_action('pops_before_page');
+do_action( 'pops_before_page' );
 
 $args = [
-  'posts_per_page' => 4,
-  'orderby' => 'post_date',
-  'order' => 'DESC',
-  'post_type' => 'post',
-  'post_status' => 'publish',
-  'suppress_filters' => true
+	'posts_per_page'   => 4,
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'post_type'        => 'post',
+	'post_status'      => 'publish',
+	'suppress_filters' => true
 ];
 
-$recent_posts = new WP_Query($args);
+$recent_posts = new WP_Query( $args );
+
+$blogPageUrl = get_permalink( get_option( 'page_for_posts' ) );
+
+$aboutPageId = get_field('about_page_link');
+
+$aboutPageUrl = $aboutPageId ? get_permalink($aboutPageId) : '/hanke';
 
 ?>
 
 
-  <section class="row main-container text-center">
+<section class="row main-container text-center">
     <div class="header-container">
-      <h1 class="margin-bottom-container"><?= get_field('header');
-                                          ?></h1>
-      <p class="header__ingress"> <?= wp_strip_all_tags(get_field('subheader'));
-                                  ?> </p>
-      <div class="header__links">
-        <a href="#" target="_blank" class="button button--header-link" data-toggle="modal" data-target="#videoModal">
-          <i class="fa fa-play" aria-hidden="true"></i>
-          <span>Katso esittelyvideo</span>
-        </a><!--
-      --><a class="button button--header-link" href="about-page">
-          <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-          <span>Lue lisää hankkeesta</span>
-        </a>
-      </div>
+        <h1 class="margin-bottom-container"><?= get_field( 'header' );
+			?></h1>
+        <p class="header__ingress"> <?= wp_strip_all_tags( get_field( 'subheader' ) );
+			?> </p>
+        <div class="header__links">
+            <a href="#" target="_blank" class="button button--header-link" data-toggle="modal"
+               data-target="#videoModal">
+                <i class="fa fa-play" aria-hidden="true"></i>
+                <span>Katso esittelyvideo</span>
+            </a><!--
+      --><a class="button button--header-link" href="<?php echo $aboutPageUrl ?>">
+                <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                <span>Lue lisää hankkeesta</span>
+            </a>
+        </div>
     </div>
-  </section>
+</section>
 </header>
 
 <style>
-  .wave {
-    background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_home.svg) repeat-x center;
-  }
+    .wave {
+        background: url(<?php echo UTILS()->get_image_uri(); ?>/slice_home.svg) repeat-x center;
+    }
 </style>
 
 <div class="wave"></div>
 
 <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <?php the_field('video') ?>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+			<?php the_field( 'video' ) ?>
+        </div>
     </div>
-  </div>
 </div>
 
 <main class="home">
-  <section class="home__links">
-    <h2 class="pull-left elem-container">Blogi</h2>
-    <a class="button button--section-link pull-right" href="blog-page">
-      <span>Kaikki artikkelit</span>
-      <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-    </a>
-  </section>
-  <section class="home__videos">
-    <?php if ($recent_posts->have_posts()) : while ($recent_posts->have_posts()) : $recent_posts->the_post();
-    get_template_part('partials/content', 'excerpt');
-    endwhile;
-    wp_reset_postdata();
-    endif;
-    ?>
-  </section>
-  <section class="home__bottom">
-    <a class="button button--section-link center-block" href="blog-page">
-      <span>Kaikki artikkelit</span>
-      <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-    </a>
-  </section>
+    <section class="home__links">
+        <h2 class="pull-left elem-container">Blogi</h2>
+        <a class="button button--section-link pull-right" href="<?php echo $blogPageUrl ?>">
+            <span>Kaikki artikkelit</span>
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+        </a>
+    </section>
+    <section class="home__videos">
+		<?php if ( $recent_posts->have_posts() ) : while ( $recent_posts->have_posts() ) : $recent_posts->the_post();
+			get_template_part( 'partials/content', 'excerpt' );
+		endwhile;
+			wp_reset_postdata();
+		endif;
+		?>
+    </section>
+    <section class="home__bottom">
+        <a class="button button--section-link center-block" href="blog-page">
+            <span>Kaikki artikkelit</span>
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+        </a>
+    </section>
 </main>
 
 <?php get_footer(); ?>
